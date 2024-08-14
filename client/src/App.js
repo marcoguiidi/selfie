4dee31a2af8dc9ff469907cb280ba8c622964b14
marco <marco.guidi24@studio.unibo.it>
2024-08-14 22:02:25 +0200

    simple fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useEffect } from'react';
+import { useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -6,6 +8,9 @@ import Home from './components/Home';
 import Logout from './components/Logout';
 import MyCalendar from './components/Calendar';
 import Navbar from './components/Navbar';
+
+
+
 
 // Funzione per verificare se l'utente è autenticato
 const isAuthenticated = () => {
@@ -21,17 +26,30 @@ const ProtectedRoute = ({ element }) => {
 };
 
 const App = () => {
+
+    useEffect(() => {
+        // Rimuove il token quando l'utente chiude o ricarica la pagina
+        const handleUnload = () => {
+            localStorage.removeItem('authToken');
+        };
+    
+        window.addEventListener('beforeunload', handleUnload);
+    
+        return () => {
+            window.removeEventListener('beforeunload', handleUnload);
+        };
+    }, []);
+
     return (
         <Router>
-            <Navbar />  {/* Navbar sempre presente */}
+            <Navbar />
             <Routes>
-                <Route path="/" element={<Navigate to="/home" replace />} />  {/* uso navigate to al posto di dare il componenete per avere la route protetta */}
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
                 <Route path="/logout" element={<ProtectedRoute element={<Logout />} />} />
                 <Route path="/calendar" element={<ProtectedRoute element={<MyCalendar />} />} />
-                <Route path='*' element={<ProtectedRoute element={<Home />} />} />
+                <Route path="*" element={<Navigate to="/home" replace />} />  {/* uso navigate to al posto di dare il componenete per avere la route protetta */}
             </Routes>
         </Router>
     );
