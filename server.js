6552f7443840e2886280ca2c63e0469e5740fcc9
marco <marco.guidi24@studio.unibo.it>
2024-09-21 15:21:54 +0200

    fixed port number

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(bodyParser.json());
